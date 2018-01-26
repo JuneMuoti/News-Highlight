8ad9df48aeffcda7ceb751533b2ece55b47a90fd
June Muoti <june.muoti@gmail.com>
2018-01-26 11:01:31 +0300

    add configuration class

---
+++ app/config.py
@@ -0,0 +1,14 @@
+class Config:
+    pass
+
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
+
+
+
+    DEBUG = True
+
+
+
--- app/views.py
+++ app/views.py
@@ -4,3 +4,7 @@ from app import app
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/news/<int:news_id>')
+def news(news_id):
+    return render_template('news.html',id=news_id)
